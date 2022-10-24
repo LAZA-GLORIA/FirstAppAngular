0a857fa5d5954f71a09437b43cd4f0ed69d084b7
LAZA-GLORIA <gloria.besamba@axyus.com>
2022-10-24 10:39:10 +0200

    first commit awesome-component branch

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ Les composants sont si présents au sein d'Angular qu'ils conditionnent jusqu'à
 
 Web components 
 https://developer.mozilla.org/fr/docs/Web/Web_Components
+
+branch awesome-component
+ng add @angular/material
+
+modules:
+ng g m core 
+ng g m shared
+ng g m social
+ng g m social-media --routing
+
+components:
+ ng g c core/components/header
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HeroesComponent } from './heroes/heroes.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
