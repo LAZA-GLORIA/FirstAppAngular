465ec02f837f792cc5bc785b0ed58468e987f78c
LAZA-GLORIA <gloria.besamba@axyus.com>
2022-10-25 12:50:41 +0200

    buble app

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { outputAst } from '@angular/compiler';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Comment } from '../../../core/models/comment.model';
 
@@ -8,17 +9,29 @@ import { Comment } from '../../../core/models/comment.model';
   styleUrls: ['./comments.component.scss'],
 })
 export class CommentsComponent implements OnInit {
+  // création d'attributs personnalisés avec les inputs
+  // ie des attributs html auxquels on vient lier les éléments grâce au input
+  // <app-comments [comments]="post.comments"></app-comments>
   @Input() comments!: Comment[];
+  // On a un output sur lequel on peut venir brancher du comportement
+  @Output() newComment = new EventEmitter<string>();
 
   commentCtrl!: FormControl;
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.commentCtrl = this.formBuilder.control('', [
+    this.commentCtrl = this.fb.control('', [
       Validators.required,
-      Validators.minLength(10),
+      Validators.minLength(10)
     ]);
   }
 
-  onLeaveComment() {}
+  // Pour le sens inverse pour lier un comportement à un évènement à l'intérieur du component
+  onLeaveComment() {
+    if (this.commentCtrl.invalid) {
+      return;
+    }
+    this.newComment.emit(this.commentCtrl.value);
+    this.commentCtrl.reset();
+  }
 }
